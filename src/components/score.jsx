9d1f78afa63797445a8db1e1c1d2524e0d6bec27
Naresh Kumar <nareshkumarr.s17@gmail.com>
2026-07-06 20:29:15 +0530

    changed style off the button

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -176,8 +176,8 @@ return(
 
             
         </div> : ""}
-       {status ===false ?  <button className="bg-red-600 text-white font-bold p-2" onClick={()=> {setstatus(true) }}>end ininings?</button> : ""}
-       {status === true ?  <button className="bg-green-600 font-black p-2 text-white" onClick={() =>{setstatus(false) , dispatch({type:"end"})}  }>start second inings</button> : ""} 
+       {status ===false ?  <button className="bg-red-600 text-white font-bold p-2 rounded-xl" onClick={()=> {setstatus(true) }}>end ininings?</button> : ""}
+       {status === true ?  <button className="bg-green-600 font-black p-2 text-white rounded-xl" onClick={() =>{setstatus(false) , dispatch({type:"end"})}  }>start second inings</button> : ""} 
 
 
 
