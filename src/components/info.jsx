b06383ee8eb15e52c96c0a94a95a4036103574f4
Naresh Kumar <nareshkumarr.s17@gmail.com>
2026-07-12 01:32:22 +0530

    updatedd info 3

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -6,8 +6,10 @@ function Info() {
      <>
        <div className="bg-red-600 text-white font-bold p-1"> 
         
-          <p className="text-justify text-sm"> Enjoy!! this is a scoreline version-1.1 - Naresh Kumar R S</p> 
+          <p className="text-justify"> Enjoy!! this is a scoreline version-1.1 - Naresh Kumar R S</p> 
+            
 
+            <h1>whats new ??</h1>
           <ul>
             <li>Added target feature</li>
             <li>Added chasing and new match feature</li>
